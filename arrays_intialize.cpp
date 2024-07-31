ee6c4de20520698292c7fde055b031f9d2b752df
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2024-07-31 11:56:51 +0530

    initial_commit

---
+++ arrays_intialize.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int arr1[5]; //array of 5 integers
+
+int arr2[5] = {1,2,3,4,5}; //array of 5 integers initialized with values
+
+int arr3[] = {1,2,3,4,5}; //array size can be inferred from the initializer list
+
+    
---
+++ arrays_iterate.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[] = {1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<<sizeof(arr)<<endl;
+}
